4877c0685ced6d1db702a6df7efdb88c89786a5f
Darja Denisenko <darja_deni@ro.ru>
2025-05-09 17:49:19 +0300

    -

--- blogicum/conftest.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent
-sys.path.insert(0, str(BASE_DIR))
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogicum.settings")
